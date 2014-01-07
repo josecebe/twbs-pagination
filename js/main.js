a571e01e99bf4f95402045f5a30b42eee08fdfcb
Eugene Simakin <eugenesimakin@mail.ru>
2014-01-07 22:59:50 +0400

    'Versions' example added

--- js/main.js
+++ js/main.js
@@ -14,6 +14,16 @@ $(document).ready(function () {
         }
     });
 
+    $('#pagination-demo-v1_0').twbsPagination({
+        totalPages: 15,
+        version: '1.0'
+    });
+
+    $('#pagination-demo-v1_1').twbsPagination({
+        totalPages: 15,
+        version: '1.1'
+    });
+
     $('#visible-pages-example').twbsPagination({
         totalPages: 35,
         visiblePages: 10,
