18f62e9fa66892170db82e8dc574b68e9c5b1044
Eugene Simakin <john-24@list.ru>
2016-10-09 16:23:24 +0300

    Update docs (alt pagination) +++ Reverting jquery.simplePagination.js to it original state

--- js/jquery.simplePagination.js
+++ js/jquery.simplePagination.js
@@ -1,12 +1,12 @@
 /**
-* simplePagination.js v1.6
-* A simple jQuery pagination plugin.
-* http://flaviusmatis.github.com/simplePagination.js/
-*
-* Copyright 2012, Flavius Matis
-* Released under the MIT license.
-* http://flaviusmatis.github.com/license.html
-*/
+ * simplePagination.js v1.6
+ * A simple jQuery pagination plugin.
+ * http://flaviusmatis.github.com/simplePagination.js/
+ *
+ * Copyright 2012, Flavius Matis
+ * Released under the MIT license.
+ * http://flaviusmatis.github.com/license.html
+ */
 
 (function($){
 
@@ -193,7 +193,7 @@
 						}
 					}
 					if (o.edges < interval.start && (interval.start - o.edges != 1)) {
-						$panel.append('<li class="disabled"><a class="ellipse">' + o.ellipseText + '</a></li>');
+						$panel.append('<li class="disabled"><span class="ellipse">' + o.ellipseText + '</span></li>');
 					} else if (interval.start - o.edges == 1) {
 						methods._appendItem.call(this, o.edges);
 					}
@@ -208,7 +208,7 @@
 					}
 
 					if (o.pages - o.edges > interval.end && (o.pages - o.edges - interval.end != 1)) {
-						$panel.append('<li class="disabled"><a class="ellipse">' + o.ellipseText + '</a></li>');
+						$panel.append('<li class="disabled"><span class="ellipse">' + o.ellipseText + '</span></li>');
 					} else if (o.pages - o.edges - interval.end == 1) {
 						methods._appendItem.call(this, interval.end);
 					}
@@ -230,7 +230,7 @@
 			if (!o.invertPageOrder) {
 				if (interval.end < o.pages && o.edges > 0) {
 					if (o.pages - o.edges > interval.end && (o.pages - o.edges - interval.end != 1)) {
-						$panel.append('<li class="disabled"><a class="ellipse">' + o.ellipseText + '</a></li>');
+						$panel.append('<li class="disabled"><span class="ellipse">' + o.ellipseText + '</span></li>');
 					} else if (o.pages - o.edges - interval.end == 1) {
 						methods._appendItem.call(this, interval.end);
 					}
@@ -244,7 +244,7 @@
 			} else {
 				if (interval.start > 0 && o.edges > 0) {
 					if (o.edges < interval.start && (interval.start - o.edges != 1)) {
-						$panel.append('<li class="disabled"><a class="ellipse">' + o.ellipseText + '</a></li>');
+						$panel.append('<li class="disabled"><span class="ellipse">' + o.ellipseText + '</span></li>');
 					} else if (interval.start - o.edges == 1) {
 						methods._appendItem.call(this, o.edges);
 					}
@@ -303,7 +303,7 @@
 				} else {
 					$linkWrapper.addClass('active');
 				}
-				$link = $('<a class="current">' + (options.text) + '</a>');
+				$link = $('<span class="current">' + (options.text) + '</span>');
 			} else {
 				$link = $('<a href="' + o.hrefTextPrefix + (pageIndex + 1) + o.hrefTextSuffix + '" class="page-link">' + (options.text) + '</a>');
 				$link.click(function(event){
@@ -356,7 +356,7 @@
 							if (event.which === 13 && val !== '') {
 								// enter to accept
 								if ((val>0)&&(val<=o.pages))
-								methods._selectPage.call(self, val - 1);
+									methods._selectPage.call(self, val - 1);
 							} else if (event.which === 27) {
 								// escape to cancel
 								$ellip.empty().html(o.ellipseText);
